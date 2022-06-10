73d537a72725c819b58a314df4acacd8497f06b5
Amsapoorani <Amsapoorani.Jeganathan@itorizon.com>
2022-06-10 14:15:51 +0530

    first commit

---
+++ controlStatement/codingchallengewhileloop.java
@@ -0,0 +1,12 @@
+package controlStatement;
+//Use pre-decrement operator in while's boolean_expression to print HELLO 5 times on to the console.
+public class codingchallengewhileloop {
+    public static void main(String[] args)
+    {
+        int i=0;
+        while (++i<=5)
+        {
+            System.out.println("HELLO");
+        }
+    }
+}
