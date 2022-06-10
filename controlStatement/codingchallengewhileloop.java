8d7950a567eb46e8682a181e742e0b135f511805
Amsapoorani <Amsapoorani.Jeganathan@itorizon.com>
2022-06-10 14:24:09 +0530

    second commit

--- controlStatement/codingchallengewhileloop.java
+++ controlStatement/codingchallengewhileloop.java
@@ -4,9 +4,14 @@ public class codingchallengewhileloop {
     public static void main(String[] args)
     {
         int i=0;
-        while (++i<=5)
+        while (++i<=5) //pre increment
         {
             System.out.println("HELLO");
+        }
+        int i1=5;
+        while(--i1>=0)  //pre decrement
+        {
+            System.out.println("HELLO1");
         }
     }
 }
